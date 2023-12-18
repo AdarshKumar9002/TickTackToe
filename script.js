837a06f8bb9f8c549dec56ff269dff825cd64ac9
AdarshKumar9002 <adarashkumar9002@gmail.com>
2023-12-18 20:51:34 +0530

    add a class name and create some variables in js

---
+++ script.js
@@ -0,0 +1,12 @@
+const playerChoice = document.querySelector(".playerChoice");
+const Box1 = document.querySelector(".game__box1");
+const Box2 = document.querySelector(".game__box2");
+const Box3 = document.querySelector(".game__box3");
+const Box4 = document.querySelector(".game__box4");
+const Box5 = document.querySelector(".game__box5");
+const Box6 = document.querySelector(".game__box6");
+const Box7 = document.querySelector(".game__box7");
+const Box8 = document.querySelector(".game__box8");
+const Box9 = document.querySelector(".game__box9");
+
+playerChoice.addEventListener("")
